d316341da073fe1213f3f285ac75b9293fdc163d
Daniel van Dijk <daniel@stentorian.io>
2023-11-13 00:57:41 +0100

    #1 Added Prettier configuration (& reformatted files).

--- src/routes/articles/404.tsx
+++ src/routes/articles/404.tsx
@@ -2,10 +2,5 @@ import { component$ } from "@builder.io/qwik";
 import { CenteredTitle } from "~/components/centered-title";
 
 export default component$(() => {
-  return (
-    <CenteredTitle
-      title="404"
-      subtitle="The article you're looking for doesn't exist."
-    />
-  );
+  return <CenteredTitle title="404" subtitle="The article you're looking for doesn't exist." />;
 });
