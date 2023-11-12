b8ec2dc4e11bf5c2302c923b32a21343dd69a5fc
Daniel van Dijk <noreply+daniel@git.vandijk.sh>
2023-11-13 00:56:10 +0100

    refactor: extract MDX metadata parser to helper module
    Refs: #1

--- src/routes/articles/index.tsx
+++ src/routes/articles/index.tsx
@@ -1,53 +1,33 @@
-import path from "path";
-import { readFile, readdir } from "fs/promises";
+import { readdir } from "fs/promises";
 import { component$, useStylesScoped$ } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
 import type { ArticleSummaryProps } from "~/components/articles/article-summary-item";
 import { ArticleSummaryList } from "~/components/articles/article-summary-list";
 
 import styles from "./index.css?inline";
+import { extractMetadataFromMdxFile } from "~/helpers/parser";
 
-export const useArticleSummaries = routeLoader$(
-  async (): Promise<Array<ArticleSummaryProps>> => {
-    const articlesPath = "./src/routes/articles";
-    const entities = await readdir(articlesPath, { withFileTypes: true });
+export const useArticleSummaries = routeLoader$(async (): Promise<Array<ArticleSummaryProps>> => {
+  const articlesPath = "./src/routes/articles";
+  const entities = await readdir(articlesPath, { withFileTypes: true });
 
-    const results = [];
+  const results = [];
 
-    for (const entity of entities) {
-      if (!entity.isDirectory()) {
-        continue;
-      }
-
-      const mdx = await readFile(
-        path.join(articlesPath, entity.name, "index.mdx"),
-        { encoding: "utf-8" }
-      );
-
-      const [, metaBlock] = mdx.split("---", 2);
-      const [, date, title, description] = metaBlock.split("\n");
-
-      const summary: Record<string, string> = {};
-
-      for (const serializedValue of [date, title, description]) {
-        const [key, ...values] = serializedValue.split(":");
-        summary[key.trim()] = values.join(":").trim();
-      }
-
-      results.push({
-        ...summary,
-        path: entity.name,
-      } as ArticleSummaryProps);
+  for (const entity of entities) {
+    if (!entity.isDirectory()) {
+      continue;
     }
 
-    return results.sort((a, b) => {
-      const dateA = new Date(a.date);
-      const dateB = new Date(b.date);
-
-      return dateB.getTime() - dateA.getTime();
-    });
+    results.push(await extractMetadataFromMdxFile(articlesPath, entity.name));
   }
-);
+
+  return results.sort((a, b) => {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+
+    return dateB.getTime() - dateA.getTime();
+  });
+});
 
 export default component$(() => {
   useStylesScoped$(styles);
