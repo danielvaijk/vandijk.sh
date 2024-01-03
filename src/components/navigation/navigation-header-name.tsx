adbdd3872af0abbfb69a50b237d4d716c5119ad6
Daniel van Dijk <daniel@stentorian.io>
2024-01-03 22:04:17 +0100

    #1 Favicon image in the header is now imported directly from source.

--- src/components/navigation/navigation-header-name.tsx
+++ src/components/navigation/navigation-header-name.tsx
@@ -1,9 +1,11 @@
 import { component$ } from "@builder.io/qwik";
 
+import ImgFavicon from "~/media/favicon.jpg?jsx";
+
 export const NavigationHeaderName = component$(() => {
   return (
     <div id="header-name">
-      <img src="/favicon.ico" width="60" height="60" />
+      <ImgFavicon decoding="sync" loading="eager" style={{ width: "60px", height: "60px" }} />
       <h2>aniel van Dijk</h2>
     </div>
   );
