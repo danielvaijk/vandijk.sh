c7c0700180bc4f2c1da7cffa6bc138af43624cd1
Daniel van Dijk <noreply+daniel@git.vandijk.sh>
2024-01-04 20:03:34 +0100

    chore: add console log with repo link
    Refs: #1

--- src/components/document/document-body.tsx
+++ src/components/document/document-body.tsx
@@ -1,7 +1,18 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useVisibleTask$ } from "@builder.io/qwik";
 import { RouterOutlet, ServiceWorkerRegister } from "@builder.io/qwik-city";
 
 export const DocumentBody = component$(() => {
+  useVisibleTask$(
+    () => {
+      console.info(
+        "Hello there! If you're looking for the source code, you can find it here: https://github.com/danielvaijk/vandijk.sh"
+      );
+    },
+    {
+      strategy: "document-ready",
+    }
+  );
+
   return (
     <body>
       <RouterOutlet />
