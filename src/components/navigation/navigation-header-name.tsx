65207ad0cc3af4b87d6e4e8f09fd24fcc63fe86a
Daniel van Dijk <noreply+daniel@git.vandijk.sh>
2024-01-05 21:09:56 +0100

    fix: enforce alt text for images
    Refs: #1

--- src/components/navigation/navigation-header-name.tsx
+++ src/components/navigation/navigation-header-name.tsx
@@ -5,7 +5,12 @@ import ImgFavicon from "~/media/favicon.jpg?jsx";
 export const NavigationHeaderName = component$(() => {
   return (
     <div id="header-name">
-      <ImgFavicon decoding="sync" loading="eager" style={{ width: "60px", height: "60px" }} />
+      <ImgFavicon
+        decoding="sync"
+        loading="eager"
+        alt="Website icon with 'D' initial"
+        style={{ width: "60px", height: "60px" }}
+      />
       <h2>aniel van Dijk</h2>
     </div>
   );
