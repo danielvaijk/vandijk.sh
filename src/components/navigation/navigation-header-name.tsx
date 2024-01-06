1aab9716ebb7e498974d460326a93ce18f7178b1
Daniel van Dijk <noreply+daniel@git.vandijk.sh>
2024-01-06 21:43:25 +0100

    style: improve mobile responsiveness
    Refs: #1

--- src/components/navigation/navigation-header-name.tsx
+++ src/components/navigation/navigation-header-name.tsx
@@ -5,12 +5,7 @@ import ImgFavicon from "~/media/favicon.jpg?jsx";
 export const NavigationHeaderName = component$(() => {
   return (
     <div id="header-name">
-      <ImgFavicon
-        decoding="sync"
-        loading="eager"
-        alt="Website icon with 'D' initial"
-        style={{ width: "60px", height: "60px" }}
-      />
+      <ImgFavicon decoding="sync" loading="eager" alt="Website icon with 'D' initial" />
       <h2>aniel van Dijk</h2>
     </div>
   );
