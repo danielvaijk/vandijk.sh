eb7e56729b3e79ac333fade4e827f7644218ca7d
Daniel van Dijk <daniel@vandijk.sh>
2024-01-07 02:19:10 +0100

    refactor: modularize global.css and inline normalization
    Refs: #1

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,4 @@
 import { component$, Slot, useStylesScoped$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
 
 import { NavigationHeader } from "~/components/navigation/navigation-header";
 import styles from "./layout.css?inline";
@@ -17,16 +16,3 @@ export default component$(() => {
     </>
   );
 });
-
-export const head: DocumentHead = {
-  links: [
-    {
-      rel: "stylesheet",
-      href: "https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/2.0.0/modern-normalize.min.css",
-      integrity:
-        "sha512-4xo8blKMVCiXpTaLzQSLSw3KFOVPWhm/TRtuPVc4WG6kUgjH6J03IBuG7JZPkcWMxJ5huwaBpOpnwYElP/m6wg==",
-      crossorigin: "anonymous",
-      referrerpolicy: "no-referrer",
-    },
-  ],
-};
