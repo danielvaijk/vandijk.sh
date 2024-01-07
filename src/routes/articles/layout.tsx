e51a5737b5a326d5a2e7d3ff3e263f9f246681d0
Daniel van Dijk <daniel@vandijk.sh>
2024-01-07 22:29:34 +0100

    perf: lazy load Prism stylesheet with custom theme
    Refs: #1

--- src/routes/articles/layout.tsx
+++ src/routes/articles/layout.tsx
@@ -1,10 +1,12 @@
 import { component$, Slot, useStyles$ } from "@builder.io/qwik";
 import { useLocation, type DocumentHead } from "@builder.io/qwik-city";
 
-import styles from "./layout.css?inline";
+import stylesForLayout from "./layout.css?inline";
+import stylesForCodeHighlights from "../../styles/prism.css?inline";
 
 export default component$(() => {
-  useStyles$(styles);
+  useStyles$(stylesForLayout);
+  useStyles$(stylesForCodeHighlights);
 
   const location = useLocation();
   const isAtRoot = location.url.pathname.endsWith("/articles/");
@@ -23,22 +25,5 @@ export const head: DocumentHead = ({ head }) => {
   const titleBase = "Daniel van Dijk's Blog";
   const title = head.title ? `${titleBase} - ${head.title}` : titleBase;
 
-  const links = [];
-
-  // We only want to include syntax highlighting for code snippets
-  // on actual article pages, which might contain the content.
-  if (head.title) {
-    links.push({
-      rel: "preload",
-      as: "style",
-      onload: "this.onload=null;this.rel='stylesheet'",
-      href: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism-tomorrow.min.css",
-      integrity:
-        "sha512-vswe+cgvic/XBoF1OcM/TeJ2FW0OofqAVdCZiEYkd6dwGXthvkSFWOoGGJgS2CW70VK5dQM5Oh+7ne47s74VTg==",
-      crossorigin: "anonymous",
-      referrerpolicy: "no-referrer",
-    });
-  }
-
-  return { title, links };
+  return { title };
 };
