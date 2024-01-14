9998c407d1803d8946500a0b07ca605e3e03b5c4
Daniel van Dijk <noreply+daniel@git.vandijk.sh>
2024-01-14 15:57:09 +0100

    feat: add homepage content
    Refs: #1

--- src/components/navigation/navigation-header-name.tsx
+++ src/components/navigation/navigation-header-name.tsx
@@ -1,12 +1,13 @@
 import { component$ } from "@builder.io/qwik";
+import { Link } from "@builder.io/qwik-city";
 
 import ImgFavicon from "~/media/favicon.jpg?jsx";
 
 export const NavigationHeaderName = component$(() => {
   return (
-    <div id="header-name">
+    <Link id="header-name" href="/" prefetch>
       <ImgFavicon decoding="sync" loading="eager" alt="Website icon with 'D' initial" />
       <h2>aniel van Dijk</h2>
-    </div>
+    </Link>
   );
 });
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,4 +1,5 @@
 import { component$, Slot, useStylesScoped$ } from "@builder.io/qwik";
+import { useLocation } from "@builder.io/qwik-city";
 
 import { NavigationHeader } from "~/components/navigation/navigation-header";
 
@@ -7,9 +8,12 @@ import styles from "./layout.css?inline";
 export default component$(() => {
   useStylesScoped$(styles);
 
+  const location = useLocation();
+  const isHomepage = location.url.pathname === "/";
+
   return (
     <>
-      <NavigationHeader />
+      {!isHomepage && <NavigationHeader />}
 
       <main>
         <Slot />
