67251304f998bedd9d58ac8c4ae996101b4f83bc
Daniel van Dijk <noreply+daniel@git.vandijk.sh>
2024-01-31 12:42:56 +0100

    refactor: separate adapter Vite config
    Refs: #1
    
    The build.client script needs to run first and separately from the build.server portion, since the former depends on the latter.

---
+++ adapters/vite.config.ts
@@ -0,0 +1,21 @@
+import { staticAdapter } from "@builder.io/qwik-city/adapters/static/vite";
+import { extendConfig } from "@builder.io/qwik-city/vite";
+
+import baseConfig from "../vite.config";
+import { determineOriginUrl } from "../src/utilities/url";
+
+export default extendConfig(baseConfig, () => {
+  return {
+    build: {
+      ssr: true,
+      rollupOptions: {
+        input: "@qwik-city-plan",
+      },
+    },
+    plugins: [
+      staticAdapter({
+        origin: determineOriginUrl(),
+      }),
+    ],
+  };
+});
