2b40ea41693de29aad51bc282ff4535407439ccf
Daniel van Dijk <noreply+daniel@git.vandijk.sh>
2024-03-22 23:50:27 +0100

    chore: simplify resume content and improve print version
    Refs: #8

--- src/components/resume/resume-section.tsx
+++ src/components/resume/resume-section.tsx
@@ -5,15 +5,22 @@ import styles from "./resume-section.css?inline";
 interface ResumeSectionProps {
   title: string;
   withoutBulletPoints?: boolean;
+  withSplitColumns?: boolean;
 }
 
 export const ResumeSection = component$<ResumeSectionProps>(
-  ({ title, withoutBulletPoints = false }) => {
+  ({ title, withoutBulletPoints = false, withSplitColumns = false }) => {
     useStylesScoped$(styles);
 
     return (
       <div
-        class={["resume-section", { "resume-section-without-bullet-points": withoutBulletPoints }]}
+        class={[
+          "resume-section",
+          {
+            "resume-section-split-columns": withSplitColumns,
+            "resume-section-without-bullet-points": withoutBulletPoints,
+          },
+        ]}
       >
         <h4>{title.toUpperCase()}</h4>
         <hr />
