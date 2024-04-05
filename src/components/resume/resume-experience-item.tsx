748cf6a1018e6d5cd50404a8ad6d6bfaad53756b
Daniel van Dijk <daniel@stentorian.io>
2024-04-05 21:18:46 +0200

    #8 Improved resume contents & print.

--- src/components/resume/resume-experience-item.tsx
+++ src/components/resume/resume-experience-item.tsx
@@ -3,29 +3,48 @@ import { component$, useStylesScoped$ } from "@builder.io/qwik";
 import styles from "./resume-experience-item.css?inline";
 
 interface ResumeExperienceItemProps {
-  company: string;
-  location: string;
-  position: string;
-  duration: string;
+  name?: string;
+  link?: string;
+  location?: string;
+  position?: string;
+  duration?: string;
   achievements?: Array<string>;
 }
 
 export const ResumeExperienceItem = component$<ResumeExperienceItemProps>(
-  ({ company, location, position, duration, achievements = [] }) => {
+  ({ name, link, location, position, duration, achievements = [] }) => {
     useStylesScoped$(styles);
 
+    const isAchievementsOnly = !name;
+    const locationOrDuration = location ?? duration;
+
+    const title = link ? (
+      <a target="_blank" rel="noopener noreferrer" href={link}>
+        {name}
+      </a>
+    ) : (
+      name
+    );
+
     return (
-      <li class="resume-experience-item">
+      <li
+        class={[
+          "resume-experience-item",
+          isAchievementsOnly && "resume-experience-item-achievements-only",
+        ]}
+      >
         <div class="resume-experience-item-content">
-          <div class="resume-experience-item-company">
-            <strong>{company}</strong>
-            <p>{location}</p>
+          <div class="resume-experience-item-name">
+            <strong>{title}</strong>
+            <p>{locationOrDuration}</p>
           </div>
 
-          <div class="resume-experience-item-position">
-            <i>{position}</i>
-            <i>{duration}</i>
-          </div>
+          {position && (
+            <div class="resume-experience-item-position">
+              <i>{position}</i>
+              <i>{duration}</i>
+            </div>
+          )}
 
           {achievements.length > 0 && (
             <ul class="resume-experience-item-achievements">
