26eebbffb42eacdd79b8c4441d221625a5af0168
Daniel van Dijk <daniel@vandijk.sh>
2024-06-18 22:55:33 +0200

    feat: display article cover images on listing page
    Refs: #11

--- src/components/articles/article-summary-item.tsx
+++ src/components/articles/article-summary-item.tsx
@@ -11,20 +11,35 @@ export interface ArticleSummaryProps {
   topic: string;
   description: string;
   readTime: number;
+  coverImageMarkup: string;
 }
 
 export const ArticleSummaryItem = component$<ArticleSummaryProps>(
-  ({ title, date, path, description, topic, readTime }) => {
+  ({ title, date, path, description, topic, readTime, coverImageMarkup }) => {
     const { scopeId } = useStylesScoped$(styles);
 
     const dateParsed = new Date(date);
     const dateFormatted = formatDateAsString(dateParsed);
+
+    // Qwik will not automatically add the scoped styles to injected HTML,
+    // so as a workaround we manually insert it as a class name to any
+    // figure, picture, and img HTML elements in the markup string.
+    const coverImageWithScopeId = coverImageMarkup.replace(
+      /<(figure|picture|img)([^>]*)>/g,
+      (_, tag, attributes) => `<${tag}${attributes} class="${scopeId}">`
+    );
 
     return (
       <li class="article-summary">
         <Link class={scopeId} href={`/blog/${path}/`} prefetch>
           <time dateTime={dateParsed.toISOString()}>{dateFormatted}</time>
           <h4>{title}</h4>
+
+          <div
+            class="article-summary-cover-image"
+            dangerouslySetInnerHTML={coverImageWithScopeId}
+          ></div>
+
           <p>{description}</p>
 
           <div class="article-summary-read-info">
