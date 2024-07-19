f6e2c47e3fe6e986ba9f401bd4dac791ccd9c4b4
Daniel van Dijk <daniel@vandijk.sh>
2024-07-20 00:35:39 +0200

    style: refine blog styling
    Refs: #14

--- src/routes/blog/index.tsx
+++ src/routes/blog/index.tsx
@@ -1,21 +1,18 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 
 import { ArticleSummaryList } from "~/components/articles/article-summary-list";
 import { CenteredTitle } from "~/components/centered-title";
 
-import styles from "./index.css?inline";
 import articles from "../../media/articles.json";
 
 export default component$(() => {
-  useStylesScoped$(styles);
-
   if (!articles.length) {
     return <CenteredTitle title="Oh!" subtitle="There's nothing here yet." />;
   }
 
   return (
     <>
-      <h1>Articles</h1>
+      <h2>Published Articles</h2>
       <ArticleSummaryList articles={articles} />
     </>
   );
