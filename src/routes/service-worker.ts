6ecd0d82e654efc2992b08c9bb47dff7c576ddc3
Daniel van Dijk <daniel@vandijk.sh>
2024-07-21 18:20:51 +0200

    fix: repair broken service worker initialization
    Refs: #17

--- src/routes/service-worker.ts
+++ src/routes/service-worker.ts
@@ -1,10 +1,11 @@
 import { setupServiceWorker } from "@builder.io/qwik-city/service-worker";
 
-declare const serviceWorker: ServiceWorkerGlobalScope;
+// eslint-disable-next-line @typescript-eslint/no-shadow -- Qwik's design...
+declare const self: ServiceWorkerGlobalScope;
 
 setupServiceWorker();
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises -- Qwik's design...
-addEventListener("install", async (): Promise<void> => serviceWorker.skipWaiting());
+addEventListener("install", async (): Promise<void> => self.skipWaiting());
 // eslint-disable-next-line @typescript-eslint/no-misused-promises -- Qwik's design...
-addEventListener("activate", async (): Promise<void> => serviceWorker.clients.claim());
+addEventListener("activate", async (): Promise<void> => self.clients.claim());
