134c34631305a12b4c2a8bc4066dd6f3d7f60e62
Daniel van Dijk <daniel@vandijk.sh>
2024-08-17 14:20:20 +0200

    chore: update visible task hook calls
    Refs: #6

--- src/components/document/document-body.tsx
+++ src/components/document/document-body.tsx
@@ -1,17 +1,15 @@
-import type { QwikJSX } from "@builder.io/qwik";
-import { component$, useVisibleTask$ } from "@builder.io/qwik";
+import { $, type QwikJSX, useOnWindow } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import { RouterOutlet, ServiceWorkerRegister } from "@builder.io/qwik-city";
 
 export const DocumentBody = component$((): QwikJSX.Element => {
-  useVisibleTask$(
-    (): void => {
+  useOnWindow(
+    "load",
+    $((): void => {
       console.info(
         "Hello there! If you're looking for the source code, you can find it here: https://github.com/danielvaijk/vandijk.sh"
       );
-    },
-    {
-      strategy: "document-ready",
-    }
+    })
   );
 
   return (
