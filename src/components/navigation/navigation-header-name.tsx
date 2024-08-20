ed1eea94e951f8dc3784a575df23f302eec8e2e2
Daniel van Dijk <daniel@vandijk.sh>
2024-08-20 19:58:22 +0200

    style: use CSS for header name initial letter icon
    Refs: #19

--- src/components/navigation/navigation-header-name.tsx
+++ src/components/navigation/navigation-header-name.tsx
@@ -2,12 +2,9 @@ import type { QwikJSX } from "@builder.io/qwik";
 import { component$ } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
 
-import ImgFavicon from "src/media/favicon.jpg?jsx";
-
 export const NavigationHeaderName = component$((): QwikJSX.Element => {
   return (
     <Link id="header-name" href="/" prefetch>
-      <ImgFavicon decoding="sync" loading="eager" alt="Website icon with 'D' initial" />
       <h2>aniel van Dijk</h2>
     </Link>
   );
